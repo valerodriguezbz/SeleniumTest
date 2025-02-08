e301277240ab5d8d024b65499adca92d3aa3e1aa
Valeria <83665852+valerodriguezbz@users.noreply.github.com>
2025-02-08 15:28:57 -0600

    test products
    - test products
    - refactored code for login

---
+++ login.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class Login:
+    def __init__(self):
+        options = webdriver.ChromeOptions()
+        options.headless = False
+        self.driver = webdriver.Chrome(options=options)
+
+    def login(self, username="standard_user", password="secret_sauce"):
+        self.driver.get("https://www.saucedemo.com/")
+        user_input = self.driver.find_element(By.ID, "user-name")
+        user_input.clear()
+        user_input.send_keys(username)
+
+        password_input = self.driver.find_element(By.ID, "password")
+        password_input.clear()
+        password_input.send_keys(password)
+
+        login_button = self.driver.find_element(By.ID, "login-button")
+        login_button.click()
+        time.sleep(3)
+
+    def tearDown(self):
+        self.driver.quit()
---
+++ test_products.py
@@ -0,0 +1,47 @@
+# This an automated test example using auce Labs Demo App (https://www.saucedemo.com/)
+# Only for practical purposes
+import unittest
+from login import Login
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+class TestProducts(unittest.TestCase):
+    # Login first
+    def setUp(self):
+        login_test = Login()
+        login_test.login()
+        self.driver = login_test.driver
+        time.sleep(3)
+
+    # After the login, test if the products are displayed
+    def test_products_visibility(self):
+        driver = self.driver
+        driver.get("https://www.saucedemo.com/inventory.html")
+        products_conatiner = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'inventory_list')
+            )
+        )
+        products = products_conatiner.find_elements(By.CLASS_NAME, 'inventory_item')
+        self.assertGreater(len(products), 0, "No hay productos")
+        time.sleep(2)
+
+    # After the login, test if the product detail is displayed
+    def test_product_details(self):
+        product = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(
+                (By.CLASS_NAME, 'inventory_item_img')
+            )
+        )
+        product.click()
+        time.sleep(2)
+        self.driver.back()
+        time.sleep(2)
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
