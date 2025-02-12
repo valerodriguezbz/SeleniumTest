261f2b8a68615bfb52ab6204d2b0578ccc5ae91a
Valeria <83665852+itsvaleriaa@users.noreply.github.com>
2025-02-12 16:31:53 -0600

    logout test

--- test_login.py
+++ test_login.py
@@ -35,3 +35,22 @@ class TestLogin(unittest.TestCase):
         )
         self.assertTrue(error_message.is_displayed())
         login_test.tearDown()
+
+    def test_logout(self):
+        login_test = Login()
+        login_test.login()
+        driver = login_test.driver
+        driver.find_element(By.CLASS_NAME,'bm-burger-button').click()
+        logout_item= WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, '//*[@id="logout_sidebar_link"]')
+            )
+        )
+        logout_item.click()
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located(
+                (By.CLASS_NAME, 'login_wrapper')
+            )
+        )
+        self.assertTrue(driver.find_element(By.CLASS_NAME,'login_wrapper'))
+        login_test.tearDown()
